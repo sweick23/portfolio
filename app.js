d77348ff8abeaabee52b6090a4d811a26d2ba5fe
Shane Weickum <sweick23@outlook.com>
2017-03-22 23:00:24 -0700

    V 1.0.0

---
+++ app.js
@@ -0,0 +1,5 @@
+function openNav(){
+    document.getElementById('mySidenav').style.width="250px";
+    document.getElementById('main').style.marginLeft="250px";
+    document.body.style.background="rgba(0,0,0,0.4)";
+}
